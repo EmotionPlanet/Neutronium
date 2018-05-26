4b3ae3b2b1c0f54e74b65c785fcc636fc835f6c6
naro143 <y.ishimi@itdharman.com>
2018-05-26 23:30:21 +0900

    add: shake x y z

--- src/page/expoApiComponents/AccelerometerApiPage/index.js
+++ src/page/expoApiComponents/AccelerometerApiPage/index.js
@@ -9,13 +9,15 @@ import styles from "./styles"
 
 export default class extends React.Component {
 
-  state = {
-    accelerometerData: {},
+  componentWillMount() {
+    this.setState({
+      count: 0,
+      accelerometerData: {},
+    })
   }
 
-  componentWillMount() {}
-
   componentDidMount() {
+    Accelerometer.setUpdateInterval(0.1);
     if (this._subscription) {
       this._unsubscribe();
     } else {
@@ -35,7 +37,19 @@ export default class extends React.Component {
 
   _subscribe = () => {
     this._subscription = Accelerometer.addListener(accelerometerData => {
-      this.setState({ accelerometerData });
+      const {x:prevX, y:prevY, z:prevZ} = this.state.accelerometerData;
+      const {x, y, z} = accelerometerData;
+      
+      let count = this.state.count + 1;
+      
+
+      this.setState(
+        { 
+          accelerometerData,
+          count,
+        }
+      );
+
     });
   }
 
@@ -45,7 +59,6 @@ export default class extends React.Component {
   }
 
   render() {
-    let { x, y, z } = this.state.accelerometerData;
 
     return (
       <Page
@@ -55,12 +68,14 @@ export default class extends React.Component {
         <Heading>Sensor States</Heading>
         <View style={styles.sensor}>
           <Text>Accelerometer:</Text>
-          <Text>x: {round(x)} y: {round(y)} z: {round(z)}</Text>
+          <Text>x: {round(this.state.prevX)} y: {round(this.state.prevY)} z: {round(this.state.prevZ)}</Text>
           <Text>Initial Accelerometer:</Text>
           <Text>
-            {round(x)}:{round(y)}:{round(z)}
+            x: {round(this.state.x)} y: {round(this.state.y)} z: {round(this.state.z)}
           </Text>
           <Text>Distance:</Text>
+          <Text>Count</Text>
+          <Text>{this.state.count}}</Text>
         </View>
       </Page>
     );
