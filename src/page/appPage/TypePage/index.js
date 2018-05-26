aafccd13d567d50d3b13b82f665da278cfefb1e8
杉山希望 <rioc.sugiyama@gmail.com>
2018-05-26 17:22:12 +0900

    add appPage

---
+++ src/page/appPage/TypePage/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Text, View, Image, Animated, ScrollView } from "react-native"
+import { Heading, Page, AnimatedSpin, AnimatedFade, AnimatedMove, FlexBox } from "react_native_template_1/src/components"
+
+import styles from "./styles"
+
+export default class extends React.Component {
+
+  componentWillMount() {
+    this.setState({
+      spinDuration: 1000,
+      fadeDuration: 2000,
+      moveDuration: 1000,
+    })
+  }
+
+  render() {
+    return (
+      <Page
+        style={styles.host}
+      >
+        <ScrollView>
+        </ScrollView>
+      </Page>
+    );
+  }
+}
