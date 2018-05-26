70a646f99a47e7f9f1b19706aa9ef2a175c08dc0
naro143 <y.ishimi@itdharman.com>
2018-05-26 17:52:03 +0900

    add: top index.js

---
+++ src/page/TopPage/index.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Text, View } from "react-native"
+import { Actions } from "react-native-router-flux"
+import { Page, Button, Heading, Avatar, FlexBox, Switch, TextInput } from "react_native_template_1/src/components"
+import { ListGroup, ListGroupItem } from "react_native_template_1/src/components/listGroup"
+import avatarImage from "react_native_template_1/assets/images/avatar.jpg"
+
+import styles from "./styles"
+
+export default class extends React.Component {
+  componentWillMount() {
+    this.setState({
+      switch1Enabled: false,
+      switch2Enabled: false,
+    })
+  }
+
+  render() {
+    return (
+      <Page> 
+        <FlexBox
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-around"
+          style={styles.host}
+        >
+          <View>
+            <Heading size="large" align="center" style={styles.title} >No Ball!</Heading>
+          </View>
+          <View>
+            <TextInput type="primary" style={styles.input} placeholder="ユーザー名"/>
+            <Button type="primary" size="large" style={styles.submit} onPress={() => undefined}>START</Button>     
+          </View>
+        </FlexBox>
+      </Page>
+    );
+  }
+}
