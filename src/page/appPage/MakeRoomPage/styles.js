0b11c7bcf0eedda2c052d0067cf876cad906a841
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-05-27 09:14:24 +0900

    modified make stye

--- src/page/appPage/MakeRoomPage/styles.js
+++ src/page/appPage/MakeRoomPage/styles.js
@@ -3,18 +3,20 @@ import { colors } from "config";
 export default {
   host: {},
   view: {
-    width: '100%',
+    position: 'relative',
+    width: '115%',
+    paddingRight: 35,
   },
   box: {
     width: '75%',
   },
   input: {
     backgroundColor: '#fff',
-    marginBottom: 40,
+    marginBottom: 45,
   },
   image: {
-    position: 'relative',
-    minWidth: '100%',
+    width: '100%',
+
   },
   submit: {
     width: '80%',
